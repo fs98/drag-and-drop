812b13fb037763f13d2a8c8e4b3ee76e041a380a
Fata Sefer <sefer.fata@gmail.com>
2022-02-06 16:49:41 +0100

    extract base component into separate file

---
+++ src/components/base-component.ts
@@ -0,0 +1,44 @@
+// Component Base Class
+namespace App {
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      const importedHTMLContent = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedHTMLContent.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBegining: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBegining ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
