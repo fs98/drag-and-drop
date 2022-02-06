fdfecc35c3871fac679e54657eea85c0531488df
Fata Sefer <sefer.fata@gmail.com>
2022-02-06 17:45:45 +0100

    refactor to use ES Modules instead of namespaces

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,44 +1,39 @@
 // Component Base Class
-namespace App {
-  export abstract class Component<
-    T extends HTMLElement,
-    U extends HTMLElement
-  > {
-    templateElement: HTMLTemplateElement;
-    hostElement: T;
-    element: U;
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string
-    ) {
-      this.templateElement = document.getElementById(
-        templateId
-      )! as HTMLTemplateElement;
-      this.hostElement = document.getElementById(hostElementId)! as T;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      const importedHTMLContent = document.importNode(
-        this.templateElement.content,
-        true
-      );
-      this.element = importedHTMLContent.firstElementChild as U;
-      if (newElementId) {
-        this.element.id = newElementId;
-      }
-
-      this.attach(insertAtStart);
+    const importedHTMLContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHTMLContent.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
     }
 
-    private attach(insertAtBegining: boolean) {
-      this.hostElement.insertAdjacentElement(
-        insertAtBegining ? "afterbegin" : "beforeend",
-        this.element
-      );
-    }
-
-    abstract configure(): void;
-    abstract renderContent(): void;
+    this.attach(insertAtStart);
   }
+
+  private attach(insertAtBegining: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBegining ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
 }
--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,19 +1,17 @@
 // autobind decorator
-namespace App {
-  export function autobind(
-    // underscore because we are telling typescript that we are aware of these variables and we need to accept it but we will not use it
-    _: any,
-    _2: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+export function autobind(
+  // underscore because we are telling typescript that we are aware of these variables and we need to accept it but we will not use it
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
